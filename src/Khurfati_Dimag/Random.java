0a2e57e938e294db81b61e126dd7411187afbdf0
Aashish Kumar Ray <65903203+Raysi2002@users.noreply.github.com>
2024-03-20 16:09:00 +0545

    Update Random.java

--- src/Khurfati_Dimag/Random.java
+++ src/Khurfati_Dimag/Random.java
@@ -1,2 +1,15 @@
-package Khurfati_Dimag;public class Random {
+package Khurfati_Dimag;
+public class Random {
+  public static String captchaGeneration(int k){
+    String str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890?&^%$#@!";
+    String captcha = "";
+    for(int i=0; i<k; i++){
+      int rand = Math.ceil(Math.random() * 70);
+      captcha += str.charAt(rand);
+    }
+    return captcha;
+  }
+  public static void main(String[] args){
+    System.out.println(captchaGeneration(k));
+  }
 }
